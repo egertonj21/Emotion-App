8ddfe4efa80c88051ab172ff3e42e83c4e37bfba
Joel Egerton <egertonjoel@gmail.com>
2024-02-15 13:24:15 +0000

    added duplicate username check to registration route

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -8,55 +8,118 @@ exports.getRegisterRoute = (req, res) => {
     res.render('register', {error: null});
 }
 
+
+
 exports.postRegisterRoute = (req, res) => {
     const { username, password, passwordCheck } = req.body;
-
+    
     // Check if the passwords match
     if (password !== passwordCheck) {
         return res.render('register', { error: 'Passwords do not match' });
     }
 
-    const apiUrl = `http://localhost:3002/emotion/add/user`;
-
-    axios.post(apiUrl, { username, password })
+    // Check if the username is already taken
+    const userApiUrl = `http://localhost:3002/emotion/username/${username}`;
+    axios.post(userApiUrl)
         .then(response => {
-            const responseData = response.data;
-            console.log(responseData);
-            if (responseData.status === 'success') {
-                req.session.isLoggedIn = true;
-                req.session.username = username; // Set username in session
-
-                // Fetch user_id based on the username
-                const userApiUrl = `http://localhost:3002/emotion/username/${username}`;
-                axios.post(userApiUrl)
-                    .then(response => {
-                        const userData = response.data;
-                        const user_id = userData.result[0].user_id;
-                        req.session.user_id = user_id; // Set user_id in session
-                        console.log(req.session);
-                        res.render('view', { error: null });
-                    })
-                    .catch(userError => {
-                        console.error('Error fetching user_id:', userError);
-                        res.render('login', { error: 'An error occurred while fetching user_id' });
-                    });
-            } else if (responseData.message === 'User not found') {
-                res.render('login', { error: "User not found" });
-            } else {
-                res.render('login', { error: "Incorrect Password" });
-            }
+            const userData = response.data;
+            return res.render('register', { error: 'Username already taken' });
         })
         .catch(error => {
-            if (error.response && error.response.status === 401) {
-                res.render('login', { error: 'Incorrect username or password' });
-            } else if (error.response && error.response.status === 404) {
-                console.log('User not found. Please check your credentials.');
-            } else {
-                console.error('Error during registration request:', error);
-                res.render('register', { error: 'An error occurred during registration' });
-            }
+            const apiUrl = `http://localhost:3002/emotion/add/user`;
+            axios.post(apiUrl, { username, password })
+                .then(response => {
+                    const responseData = response.data;
+                    console.log(responseData);
+                    if (responseData.status === 'success') {
+                        req.session.isLoggedIn = true;
+                        req.session.username = username; // Set username in session
+
+                        // Fetch user_id based on the username
+                        axios.post(userApiUrl)
+                            .then(response => {
+                                const userData = response.data;
+                                // Check if the result array is not empty before accessing user_id
+                                const user_id = userData.result.length > 0 ? userData.result[0].user_id : null;
+                                req.session.user_id = user_id; // Set user_id in session
+                                console.log(req.session);
+                                res.render('view', { error: null });
+                            })
+                            .catch(userError => {
+                                console.error('Error fetching user_id:', userError);
+                                res.render('login', { error: 'An error occurred while fetching user_id' });
+                            });
+                    } else if (responseData.message === 'User not found') {
+                        res.render('login', { error: "User not found" });
+                    } else {
+                        res.render('login', { error: "Incorrect Password" });
+                    }
+                })
+                .catch(registrationError => {
+                    if (registrationError.response && registrationError.response.status === 401) {
+                        res.render('login', { error: 'Incorrect username or password' });
+                    } else if (registrationError.response && registrationError.response.status === 404) {
+                        console.log('User not found. Please check your credentials.');
+                    } else {
+                        console.error('Error during registration request:', registrationError);
+                        res.render('register', { error: 'An error occurred during registration' });
+                    }
+                });
         });
-}
+};
+
+
+
+
+
+// exports.postRegisterRoute = (req, res) => {
+//     const { username, password, passwordCheck } = req.body;
+//     // Check if the passwords match
+//     if (password !== passwordCheck) {
+//         return res.render('register', { error: 'Passwords do not match' });
+//     }
+
+//     const apiUrl = `http://localhost:3002/emotion/add/user`;
+
+//     axios.post(apiUrl, { username, password })
+//         .then(response => {
+//             const responseData = response.data;
+//             console.log(responseData);
+//             if (responseData.status === 'success') {
+//                 req.session.isLoggedIn = true;
+//                 req.session.username = username; // Set username in session
+
+//                 // Fetch user_id based on the username
+//                 const userApiUrl = `http://localhost:3002/emotion/username/${username}`;
+//                 axios.post(userApiUrl)
+//                     .then(response => {
+//                         const userData = response.data;
+//                         const user_id = userData.result[0].user_id;
+//                         req.session.user_id = user_id; // Set user_id in session
+//                         console.log(req.session);
+//                         res.render('view', { error: null });
+//                     })
+//                     .catch(userError => {
+//                         console.error('Error fetching user_id:', userError);
+//                         res.render('login', { error: 'An error occurred while fetching user_id' });
+//                     });
+//             } else if (responseData.message === 'User not found') {
+//                 res.render('login', { error: "User not found" });
+//             } else {
+//                 res.render('login', { error: "Incorrect Password" });
+//             }
+//         })
+//         .catch(error => {
+//             if (error.response && error.response.status === 401) {
+//                 res.render('login', { error: 'Incorrect username or password' });
+//             } else if (error.response && error.response.status === 404) {
+//                 console.log('User not found. Please check your credentials.');
+//             } else {
+//                 console.error('Error during registration request:', error);
+//                 res.render('register', { error: 'An error occurred during registration' });
+//             }
+//         });
+// }
 
 
 exports.postLoginRoute = (req, res) => {
